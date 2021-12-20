c5774f3f40bbf24bf6d0fd8a4fecf5e5250e5884
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2021-12-20 22:05:57 +0300

    First Foldering

---
+++ BasicToDO/BasicToDO/BasicToDOApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicToDOApp.swift
+//  BasicToDO
+//
+//  Created by mehmet karanlık on 20.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct BasicToDOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
